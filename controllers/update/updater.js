f688e4e88b18ce715393de80a9a5bed2abb9a064
l1ghtg3m <huy.189.189@gmail.com>
2021-12-20 21:27:04 +0700

    a comment

--- controllers/update/updater.js
+++ controllers/update/updater.js
@@ -1,10 +1,10 @@
 const cron = require('node-cron');
 const updateStaticalInfo = require('./updateStaticalInfo');
 const updateAddresses = require('./updateAddresses');
-cron.schedule('1 * * * *', function() {
+cron.schedule('1 * * * *', function() { //every 1 min
     console.log('running a task every hour');
     updateStaticalInfo();
     updateAddresses();
   });
   module.exports = cron;
-// Real world : server when update at 23:59
+// Real world : server update at 23:59
