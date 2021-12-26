9129610e4d32bb24f4a91d17ce5dbd46119426c1
l1ghtg3m <huy.189.189@gmail.com>
2021-12-26 20:39:31 +0700

    chane_updater,js

--- controllers/update/updater.js
+++ controllers/update/updater.js
@@ -1,10 +1,10 @@
 const cron = require('node-cron');
 const updateStaticalInfo = require('./updateStaticalInfo');
-// const updateAddresses = require('./updateAddresses');
-cron.schedule(' 1 * * * *', function() { //every 1 min
+const updateAddresses = require('./updateAddresses');
+cron.schedule(' 1 * * * * *', function() { //every 1 min
     console.log('running a task every hour');
     updateStaticalInfo.refreshStatisticsData();
-    // updateAddresses();
+    updateAddresses();
   });
   module.exports = cron;
 // Real world : server update at 23:59
